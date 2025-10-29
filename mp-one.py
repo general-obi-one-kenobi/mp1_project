6b7a45ba8f7aa0521bd274ea98b0b8e6be3f09b6
general-obi-one-kenobi <aadi.aiyer@gmail.com>
2025-10-29 12:27:35 -0400

    Update mp-one.py

--- mp-one.py
+++ mp-one.py
@@ -1,1 +1,3 @@
-print('hello world')
+response1=input('Your friend goes up to you and asks you: Who was in Paris?\n How do you respond? \nA: Germans in WWII \nB: Parisians \nC: Both A and B\n')
+if response1 == "A":
+    print("Ok")
